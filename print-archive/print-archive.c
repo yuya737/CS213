9e3e154b135e9a49f483341727e43dc41d20182d
yuya737 <kawakami@grinnell.edu>
2020-02-15 14:31:27 -0600

    Finishing print-archive

--- print-archive/print-archive.c
+++ print-archive/print-archive.c
@@ -82,11 +82,24 @@ void print_contents(uint8_t* data, size_t size) {
 
     // Skip the first 8 characters
     data = data + 8;
+    /*
+    int count = 0;
+    while(*data !=0 && count<500){
+        printf("count: %d ascii: %d, data:%c\n",count,*data, (char) *data);
+        count++;
+        data++;
+    }
+
+    printf("count: %d ascii: %d, data:%c\n",count,*data, (char) *data);
+    return;
+    */ 
 
     while (*data != 0){
         // Allocate space for file identifier
-        char identifier[16];
+        char identifier[17];
+        identifier[16] = '\0';
         memmove(identifier, data, 16);
+        if (strlen(identifier) != 16) break; 
         // Find the backslash and change to null terminating character
         char* endChar = strchr(identifier, '/');
         *endChar = '\0';
@@ -96,18 +109,21 @@ void print_contents(uint8_t* data, size_t size) {
         data = data + 48;
 
         // File size is at most 10 bytes
-        char fileSize[10];
+        char fileSize[11];
+        fileSize[10] = '\0';
         memmove(fileSize, data, 10);
         int sizeOfFile = atoi(fileSize);
 
         // Move to File data
         data = data + 12;
-        char file[sizeOfFile];
+        char file[sizeOfFile+1];
+        file[sizeOfFile] = '\0';
         memmove(file, data, sizeOfFile);
         printf("%s\n", file);
 
         //printf("SizeofFile: %d\n", sizeOfFile);
         data = data + sizeOfFile;
+        if (sizeOfFile % 2 == 1) data++;
         //printf("%s\n", data);
 
         //printf("count: %d data:%d\n",count, *data);
