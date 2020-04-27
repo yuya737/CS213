140da961b1155db1e778f7d940f3758425129590
Kawakami <kawakami@demorgan.cs.grinnell.edu>
2020-04-27 17:28:09 -0500

    adding parallel data final

--- parallel-data-structures/queue.cc
+++ parallel-data-structures/queue.cc
@@ -5,85 +5,96 @@
 // Initialize a new queue
 void queue_init(my_queue_t *queue)
 {
-  pthread_mutex_init(&queue->lock, NULL);
-  pthread_mutex_lock(&queue->lock);
-  queue->size = 0;
-  queue->last = NULL;
-  pthread_mutex_unlock(&queue->lock);
+    // Initialize and lock lock and intialize the queue with appropriate information
+    pthread_mutex_init(&queue->lock, NULL);
+    pthread_mutex_lock(&queue->lock);
+    queue->size = 0;
+    queue->last = NULL;
+    pthread_mutex_unlock(&queue->lock);
 }
 
 // Destroy a queue
 void queue_destroy(my_queue_t *queue)
 {
-  pthread_mutex_lock(&queue->lock);
-  node_t *cur = queue->last;
-  while (cur != NULL)
-  {
-    node_t *next = cur->next;
-    free(cur);
-    cur = next;
-  }
-  pthread_mutex_unlock(&queue->lock);
+    // Lock the lock and traverse the queue, deleting each value
+    pthread_mutex_lock(&queue->lock);
+    node_t *cur = queue->last;
+    while (cur != NULL)
+    {
+        node_t *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    // unlock
+    pthread_mutex_unlock(&queue->lock);
 }
 
 // Put an element at the end of a queue
 void queue_put(my_queue_t *queue, int element)
 {
-  node_t *newNode = (node_t *)malloc(sizeof(node_t));
-  newNode->data = element;
-  newNode->next = NULL;
+    // Allocate space for a node, assign appropriate values
+    node_t *newNode = (node_t *)malloc(sizeof(node_t));
+    newNode->data = element;
+    newNode->next = NULL;
 
-  pthread_mutex_lock(&queue->lock);
-  node_t *cur = queue->last;
+    // Lock the lock and traverse to the end of list and append new node
+    pthread_mutex_lock(&queue->lock);
+    node_t *cur = queue->last;
 
-  if (cur == NULL)
-  {
-    queue->last = newNode;
-  }
-  else
-  {
-    node_t *trailCur = queue->last;
-    while (cur != NULL)
+    if (cur == NULL)
     {
-      trailCur = cur;
-      cur = cur->next;
+        queue->last = newNode;
     }
-    trailCur->next = newNode;
-  }
-  queue->size++;
-  pthread_mutex_unlock(&queue->lock);
+    else
+    {
+        node_t *trailCur = queue->last;
+        while (cur != NULL)
+        {
+            trailCur = cur;
+            cur = cur->next;
+        }
+        trailCur->next = newNode;
+    }
+    // Increment size and unlock the lock
+    queue->size++;
+    pthread_mutex_unlock(&queue->lock);
 }
 
 // Check if a queue is empty
 bool queue_empty(my_queue_t *queue)
 {
-  pthread_mutex_lock(&queue->lock);
-  bool ret = false;
-  if (queue->size == 0)
-  {
-    ret = true;
-  }
-  pthread_mutex_unlock(&queue->lock);
-  return ret;
+    // Lock the lock and check if the queue is of size 0 or not
+    pthread_mutex_lock(&queue->lock);
+    bool ret = false;
+    if (queue->size == 0)
+    {
+        ret = true;
+    }
+    // Unlock and return
+    pthread_mutex_unlock(&queue->lock);
+    return ret;
 }
 
 // Take an element off the front of a queue
 int queue_take(my_queue_t *queue)
 {
-  int ret;
-  pthread_mutex_lock(&queue->lock);
-  if (queue->size == 0)
-  {
-    ret = -1;
-  }
-  else
-  {
-    node_t *newLast = queue->last->next;
-    ret = queue->last->data;
-    free(queue->last);
-    queue->last = newLast;
-    queue->size--;
-  }
-  pthread_mutex_unlock(&queue->lock);
-  return ret;
+    int ret;
+    // Lock the lock, if the size is 0 return -1, an error
+    pthread_mutex_lock(&queue->lock);
+    if (queue->size == 0)
+    {
+        ret = -1;
+    }
+    else
+    {
+        // Take the last element and assign the new last. Free the element and decrement the size
+        node_t *newLast = queue->last->next;
+        ret = queue->last->data;
+        free(queue->last);
+        queue->last = newLast;
+        queue->size--;
+    }
+    // Unlock and return
+    pthread_mutex_unlock(&queue->lock);
+    return ret;
 }
--- parallel-data-structures/stack.cc
+++ parallel-data-structures/stack.cc
@@ -4,62 +4,69 @@
 
 // Initialize a stack
 void stack_init(my_stack_t *stack) {
-  pthread_mutex_init(&stack->lock, NULL);
-  pthread_mutex_lock(&stack->lock);
-  stack->size = 0;
-  stack->top = NULL;
-  pthread_mutex_unlock(&stack->lock);
+    // Lock the mutex lock and initialize the size and top fields
+    pthread_mutex_init(&stack->lock, NULL);
+    pthread_mutex_lock(&stack->lock);
+    stack->size = 0;
+    stack->top = NULL;
+    // unlock
+    pthread_mutex_unlock(&stack->lock);
 }
 
 // Destroy a stack
 void stack_destroy(my_stack_t *stack) {
-  pthread_mutex_lock(&stack->lock);
-  node_t *cur = stack->top;
-  while (cur != NULL) {
-    node_t *next = cur->next;
-    free(cur);
-    cur = next;
-  }
-  pthread_mutex_unlock(&stack->lock);
+    // Lock the mutex lock, traverse through the stack and destroy nodes
+    pthread_mutex_lock(&stack->lock);
+    node_t *cur = stack->top;
+    while (cur != NULL) {
+        node_t *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    // unlock
+    pthread_mutex_unlock(&stack->lock);
 }
 
 // Push an element onto a stack
 void stack_push(my_stack_t *stack, int element) {
-  node_t *newNode = (node_t *)malloc(sizeof(node_t));
-  newNode->data = element;
-
-  pthread_mutex_lock(&stack->lock);
-  newNode->next = stack->top;
-  stack->top = newNode;
-  stack->size++;
-  pthread_mutex_unlock(&stack->lock);
+    // Alocate space for a new node and assign values
+    node_t *newNode = (node_t *)malloc(sizeof(node_t));
+    newNode->data = element;
+    // Lock the lock and add node to the head
+    pthread_mutex_lock(&stack->lock);
+    newNode->next = stack->top;
+    stack->top = newNode;
+    stack->size++;
+    pthread_mutex_unlock(&stack->lock);
 }
 
 // Check if a stack is empty
 bool stack_empty(my_stack_t *stack) {
-  pthread_mutex_lock(&stack->lock);
-  bool ret = false;
-  if (stack->size == 0) {
-    ret = true;
-  }
-  pthread_mutex_unlock(&stack->lock);
-  return ret;
+    // Lock the lock and check if the size is 0 or not
+    pthread_mutex_lock(&stack->lock);
+    bool ret = false;
+    if (stack->size == 0) {
+        ret = true;
+    }
+    pthread_mutex_unlock(&stack->lock);
+    return ret;
 }
 
 // Pop an element off of a stack
 int stack_pop(my_stack_t *stack) {
-  int ret;
-  pthread_mutex_lock(&stack->lock);
-  if (stack->size == 0) {
-    ret = -1;
-  } else {
-    node_t *newHead = stack->top->next;
-    ret = stack->top->data;
-    free(stack->top);
-    stack->top = newHead;
-    stack->size--;
-  }
+    int ret;
+    // Lock the lock and find the element to pop. Store the value to return and unlock.
+    pthread_mutex_lock(&stack->lock);
+    if (stack->size == 0) {
+        ret = -1;
+    } else {
+        node_t *newHead = stack->top->next;
+        ret = stack->top->data;
+        free(stack->top);
+        stack->top = newHead;
+        stack->size--;
+    }
 
-  pthread_mutex_unlock(&stack->lock);
-  return ret;
+    pthread_mutex_unlock(&stack->lock);
+    return ret;
 }
